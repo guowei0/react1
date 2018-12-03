9f4a27d54b1b3cb045713996f3eb81968c2d6f27
guowei <913584841@qq.com>
2018-12-03 15:04:54 +0800

    ome

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+        <div className="App">
+			<main className="main">
+				
+			</main>
+        </div>
+        );
+    }
+}
+
+export default App;
